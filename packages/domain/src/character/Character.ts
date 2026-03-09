d2ba5984b59457933b704f2b4e69473081407949
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-03-09 08:30:48 -0300

    chore: update VSCode settings for TypeScript and JavaScript formatting and improve file watcher configuration

--- packages/domain/src/character/Character.ts
+++ packages/domain/src/character/Character.ts
@@ -2,47 +2,23 @@ import {
   getArchetypePreset,
   type Archetype,
 } from "@domain/archetypes/Archetypes";
-import type { PrimaryAttributes } from "@domain/attributes/primary";
-
-// export type Direction = "up" | "down" | "left" | "right";
-// export type TilePosition = { tileX: number; tileY: number };
+import {
+  createPrimaryAttributes,
+  type PrimaryAttributes,
+} from "@domain/attributes/primary";
 
 export class Character {
   public readonly archetype: Archetype;
-  public readonly attributes: PrimaryAttributes;
-  // public direction!: Direction;
-  // public position!: TilePosition;
+  public readonly attributes: {
+    primary: PrimaryAttributes;
+  };
 
   constructor(archetype: Archetype) {
-    const archetypePreset = getArchetypePreset(archetype);
-    this.archetype = archetypePreset.archetype;
-    this.attributes = archetypePreset.attributes.primary;
+    const preset = getArchetypePreset(archetype);
+
+    this.archetype = preset.archetype;
+    this.attributes = {
+      primary: createPrimaryAttributes(preset.attributes.primary),
+    };
   }
-
-  // public spawn(position: TilePosition, direction: Direction): void {
-  //   this.position = { ...position };
-  //   this.direction = direction;
-  // }
-
-  // public move(direction: Direction): void {
-  //   this.direction = direction;
-  //   switch (direction) {
-  //     case "up":
-  //       this.position.tileY -= 1;
-  //       break;
-  //     case "down":
-  //       this.position.tileY += 1;
-  //       break;
-  //     case "left":
-  //       this.position.tileX -= 1;
-  //       break;
-  //     case "right":
-  //       this.position.tileX += 1;
-  //       break;
-  //   }
-  // }
-}
-
-for (const archetype of ["warrior", "lancer", "archer", "monk"]) {
-  console.log(new Character(archetype as Archetype));
 }
