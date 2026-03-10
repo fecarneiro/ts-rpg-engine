20e98e4c403c7cd3d2332f1a8744352653357db2
Felipe Carneiro <fe.iacarneiro@gmail.com>
2026-03-10 11:36:17 -0300

    refactor: update asset paths in UI configuration and remove unused image assets from the project

--- packages/game/src/configs/ui.ts
+++ packages/game/src/configs/ui.ts
@@ -6,18 +6,18 @@ export type UIAsset = {
 export const UI_ASSETS = {
   panel: {
     key: "ui-panel",
-    url: "/assets/ui/PNG/panel_beige.png",
+    url: "/assets/ui/panel_beige.png",
   },
   buttonNormal: {
     key: "ui-button-normal",
-    url: "/assets/ui/PNG/buttonLong_beige.png",
+    url: "/assets/ui/buttonLong_beige.png",
   },
   buttonPressed: {
     key: "ui-button-pressed",
-    url: "/assets/ui/PNG/buttonLong_beige_pressed.png",
+    url: "/assets/ui/buttonLong_beige_pressed.png",
   },
   iconCross: {
     key: "ui-icon-cross",
-    url: "/assets/ui/PNG/iconCross_beige.png",
+    url: "/assets/ui/iconCross_beige.png",
   },
 } as const;
